61011df629da85a960843ef8213bc518aa706033
Yong Jin Lee <yongjin.lee0503@gmail.com>
2020-04-03 15:49:34 -0400

    Updated Junit test
    Testing all data fields within the input file.

--- CIT591FinalProject/src/ReviewReaderJunitTest.java
+++ CIT591FinalProject/src/ReviewReaderJunitTest.java
@@ -19,6 +19,7 @@ public class ReviewReaderJunitTest {
 	static ReviewReader reviewReader;
 	static ArrayList<Review> reviews;
 
+	//test if the # of records match with what's in csv file.
 	@Test
 	public void testNumberOfReviews() throws IOException {
 		reviewReader = new ReviewReader();
@@ -28,7 +29,8 @@ public class ReviewReaderJunitTest {
 		int actualNumberOfRecords = 23486;
 		assertEquals(actualNumberOfRecords, reviews.size(),  "# of records do not match!" );
 	}
-
+	
+	//test if the data are being ingested correctly.
 	@Test
 	public void testDataExtraction() {
 		Review review1 = reviews.get(0);
@@ -38,6 +40,19 @@ public class ReviewReaderJunitTest {
 		assertEquals("0", review1.getId(), "ID data extraction does not work!");
 		assertEquals("1080", review6.getClothingId(), "ClothingID extracition does not work!");
 		assertEquals(44, review14.getAge(), "Age does not work!");
+		assertEquals("", review1.getTitle(), "Null Value is not being handled correclty.");
+		assertEquals("Not for the very petite", review6.getTitle(), "Title not being inserted correctly.");
+		assertEquals("Absolutely wonderful - silky and sexy and comfortable", review1.getReviewText(), 
+				"Review Text is not being inserted correctly");
+		assertEquals(4, review1.getRating(), "Rating insertion does not work properly");
+		assertEquals(5, review14.getRating(), "Rating insertion does not work properly");
+		assertEquals(true, review1.getRecommendedInd(), "True value not being inserted correctly");
+		assertEquals(false, review6.getRecommendedInd(), "False value not being inserted correctly");
+		assertEquals(4, review6.getPositiveFeedback(), "Positive feed back not being inserted correctly");
+		assertEquals("General", review6.getDivisionName(), "Divisiosn  name not being inserted correclty");
+		assertEquals("Dresses", review6.getDepartmentName(), "Department info not being inserted correctly.");
+		assertEquals("Intimates", review14.getClassName(), "Class Name data not being inserted correctly");
+		
 		
 	}
 
