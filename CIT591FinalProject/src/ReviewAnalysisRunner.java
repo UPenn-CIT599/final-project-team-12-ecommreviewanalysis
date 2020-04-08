18a104458af04ab82062f2aecca3a337808133dd
Xinyi Samore <44687589+claireche89@users.noreply.github.com>
2020-04-08 17:33:31 +0800

    imported corenlp and csv commons

--- CIT591FinalProject/src/ReviewAnalysisRunner.java
+++ CIT591FinalProject/src/ReviewAnalysisRunner.java
@@ -2,23 +2,31 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 /**
- * This is for us to run our different java code class in one main location executing sequence of actions we need.
- * We will read the data, analyze, and output the data over here.
+ * This is for us to run our different java code class in one main location
+ * executing sequence of actions we need. We will read the data, analyze, and
+ * output the data over here.
+ * 
  * @author YongJin, Xinyi, Xiting
  *
  */
 public class ReviewAnalysisRunner {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ReviewReader reviewReader = new ReviewReader();
-		try {
-			reviewReader.readFile();
-		} catch (IOException e) {
-			System.out.println("Could not find the input file");
-		}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        ReviewReader reviewReader = new ReviewReader();
+       
+        try {
+            reviewReader.readFile();
+ ArrayList<Review> reviews= new ArrayList<Review>();
+        reviews=reviewReader.getReviews();
+       
+        ReviewAnalysis ra= new ReviewAnalysis(reviews); 
+        double median=ra.medianAgeOfMostPopular();
+        System.out.println(median);
+        } catch (IOException e) {
+            System.out.println("Could not find the input file");
+        }
 
-	}
-
+    }
 
 }
