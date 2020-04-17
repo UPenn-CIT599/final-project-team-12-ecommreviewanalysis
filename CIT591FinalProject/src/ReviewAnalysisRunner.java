07ef0a3aff11f87488900472fa459f8773fb8373
Xinyi Samore <44687589+claireche89@users.noreply.github.com>
2020-04-17 13:51:47 +0800

    Update ReviewAnalysisRunner.java

--- CIT591FinalProject/src/ReviewAnalysisRunner.java
+++ CIT591FinalProject/src/ReviewAnalysisRunner.java
@@ -12,35 +12,48 @@ import java.util.HashMap;
  */
 public class ReviewAnalysisRunner {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ReviewReader secondRR = new ReviewReader("Sentiment_Analysis_Output.csv", true);
-		try {
-			secondRR.readFile();
-			ArrayList<Review> newValidReviews = secondRR.getValidReviews();
-			ReviewAnalysis ra = new ReviewAnalysis(newValidReviews);
-			
-			System.out.println("The average age of positive reviews is " + ra.getAveAgeOfPosReviews());
-			System.out.println();
-			
-			System.out.println("The median age of positive reviews is " + ra.getMedianAgeOfPosReviews());
-			System.out.println("\n");
-			
-			System.out.println("Model with best overall reviews in each class:");
-			ra.findModelWithKeywords(1);
-			System.out.println("\n");
-			
-			System.out.println("Model with worst overall reviews in each class:");
-			ra.findModelWithKeywords(-1);
-			System.out.println("\n");
-	
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println("Oops...Something went wrong with csvParser");
-		}
-	
-	}
+        ReviewReader secondRR = new ReviewReader("Sentiment_Analysis_Output.csv", true);
+        try {
+            secondRR.readFile();
+            ArrayList<Review> newValidReviews = secondRR.getValidReviews();
+            ReviewAnalysis ra = new ReviewAnalysis(newValidReviews);
+
+            System.out.println("The average age of positive reviews is " + ra.getAveAgeOfPosReviews());
+            System.out.println();
+
+            System.out.println("The median age of positive reviews is " + ra.getMedianAgeOfPosReviews());
+            System.out.println("\n");
+            
+            System.out.println("Clothing ID " + ra.productWithMostReviews() + " has the most number of reviews.");
+            System.out.println(
+                    "The average age of customers for the most popular item is " + ra.averageAgeOfMostPopular());
+            System.out
+            .println("The median age of customers for the most popular item is " + ra.medianAgeOfMostPopular());
+            System.out.println("\n");
+
+            System.out.println("Model with best overall reviews in each class:");
+            ra.findModelWithKeywords(1);
+            System.out.println("\n");
+
+            System.out.println("Model with worst overall reviews in each class:");
+            ra.findModelWithKeywords(-1);
+            System.out.println("\n");
+
+            System.out.println(ra.numberOfReviews());
+            System.out.println("\n");
+
+            Plots plot = new Plots(newValidReviews);
+            plot.plotHistogram1();
+            plot.plotHistogram2();
+            plot.plotPieChart();
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println("Oops...Something went wrong with csvParser");
+        }
 
     }
 
