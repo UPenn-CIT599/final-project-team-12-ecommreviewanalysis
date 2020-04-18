5baada6444d8c7b33c3e3b4c2afb52a548424f5d
xitingtz <xiting@seas.upenn.edu>
2020-04-18 23:12:06 +0800

    write txt report

--- CIT591FinalProject/src/AnalysisResultFileWriter.java
+++ CIT591FinalProject/src/AnalysisResultFileWriter.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 /**
  * This class write the result of our analysis as txt output in formatted manner.
  * @author Xinyi, Xiting, YongJin
@@ -6,10 +11,21 @@
 public class AnalysisResultFileWriter {
 	private String OutputFileName;
 	
-	//this will contain the actions it needs to take on writing the output file.
-	public static void writeOutputFile() {
+	/**
+	 * Writes a .txt of the results
+	 */
+	public void writeReport(ArrayList<String> results){
+		File out = new File("report.txt");
 		
+		try( PrintWriter pw = new PrintWriter(out) ) {
+			
+			for( String r : results ) {
+				pw.println(r);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Could not write the File out.  Check permissions, or contact course staff for help");
+		}
 	}
-
-
 }
