bcdfcfe7c533a63121c92d58324774c7e96d6c73
xitingtz <xiting@seas.upenn.edu>
2020-04-18 22:12:46 +0800

    updated AnalysisRunner, AnalysisResultFileWriter, minor change on review analysis

--- CIT591FinalProject/src/AnalysisResultFileWriter.java
+++ CIT591FinalProject/src/AnalysisResultFileWriter.java
@@ -1,15 +1,38 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 /**
  * This class write the result of our analysis as txt output in formatted manner.
  * @author Xinyi, Xiting, YongJin
  *
  */
 public class AnalysisResultFileWriter {
-	private String OutputFileName;
 	
+<<<<<<< Updated upstream
 	//this will contain the actions it needs to take on writing the ouptut file.
 	public static void writeOutputFile() {
+=======
+	/**
+	 * Writes a .txt of the answers
+	 */
+	public void writeReport(ArrayList<String> results){
+		File out = new File("report.txt");
+		
+		try( PrintWriter pw = new PrintWriter(out) ) {
+			
+			for( String r : results ) {
+				pw.println(r);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Could not write the File out.  Check permissions, or contact course staff for help");
+		}
+>>>>>>> Stashed changes
 		
 	}
-
+		
 
 }
--- CIT591FinalProject/src/ReviewAnalysisRunner.java
+++ CIT591FinalProject/src/ReviewAnalysisRunner.java
@@ -12,6 +12,7 @@ public class ReviewAnalysisRunner {
 
 	public static void main(String[] args) {
 
+<<<<<<< Updated upstream
 		ReviewReader secondRR = new ReviewReader("Sentiment_Analysis_Output.csv", true);
 		try {
 			secondRR.readFile();
@@ -41,4 +42,70 @@ public class ReviewAnalysisRunner {
 	}
 
 
+=======
+    public static void main(String[] args) {
+
+        ReviewReader secondRR = new ReviewReader("Sentiment_Analysis_Output.csv", true);
+        try {
+            secondRR.readFile();
+            ArrayList<Review> newValidReviews = secondRR.getValidReviews();
+            ReviewAnalysis ra = new ReviewAnalysis(newValidReviews);
+            AnalysisResultFileWriter arfw = new AnalysisResultFileWriter();
+            ArrayList<String> report = new ArrayList<String>();
+            
+            System.out.println("We are generating the analysis report for you...");
+            System.out.println("It may take a few minutes, thanks for your patience." + "\n");
+           
+            String result1 = "The average age of positive reviews is " + ra.getAveAgeOfPosReviews();
+            report.add(result1 + "\n");
+
+            String result2 = "The median age of positive reviews is " + ra.getMedianAgeOfPosReviews();
+            report.add(result2 + "\n");
+            
+            report.add("Clothing ID " + ra.productWithMostReviews() 
+            				+ " has the most number of reviews.");
+            report.add( "The average age of customers for the most popular item is " 
+            				+ ra.averageAgeOfMostPopular());
+            report.add("The median age of customers for the most popular item is " 
+            				+ ra.medianAgeOfMostPopular());
+            report.add("");
+            
+            report.add("In each class, the model with best reviews and the keywords in its reviews are:");
+            ArrayList<String> bestReviewModel = ra.findModelWithKeywords(1);
+            for (int i = 0; i < bestReviewModel.size(); i++) {
+            	report.add(bestReviewModel.get(i));
+            }
+            report.add("");
+            
+            report.add("In each class, the model with worst reviews and the keywords in its reviews are:");
+            ArrayList<String> worstReviewModel = ra.findModelWithKeywords(-1);
+            for (int i = 0; i < worstReviewModel.size(); i++) {
+            	report.add(worstReviewModel.get(i));
+            }
+            report.add("");
+            
+            report.add(ra.numberOfReviews());
+            
+            arfw.writeReport(report);
+            
+            System.out.println("Report is ready!" + "\n");
+            
+            System.out.println("Generating charts..." + "\n");
+            
+            Plots plot = new Plots(newValidReviews);
+            plot.plotHistogram1();
+            plot.plotHistogram2();
+            plot.plotPieChart();
+            
+            System.out.println();
+            
+            System.out.println("Contact us if you need more analysis reports!");       
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println("Oops...Something went wrong with csvParser");
+        }
+    }
+>>>>>>> Stashed changes
 }
